5ac01daf4f56c726493e6cd06adffad54d1e49ad
blueapple825 <blueapple8259@gmail.com>
2024-05-12 00:58:13 +0900

    배경 효과 구현 방식 개선

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const bodyElement = document.querySelector("body");
+const backgroundElement = document.querySelector(".top");
 const fileInput = document.getElementById("fileUpload");
 let mousePos = [0, 0];
 let bgGradientPos = [0, 0];
 let lastFrameTime = performance.now();
-
-bodyElement.addEventListener("mousemove",function(e){
-    mousePos[0] = e.pageX;
-    mousePos[1] = e.pageY;
-})
 
 fileInput.onchange = () => {
     const selectedFile = fileInput.files[0];
@@ -62,16 +58,21 @@ fileInput.onchange = () => {
     reader.readAsText(selectedFile);
 };
 
+bodyElement.addEventListener("mousemove",function(e){
+    mousePos[0] = e.pageX;
+    mousePos[1] = e.pageY;
+})
+
 function backgroundAnimation() {
     const currentTime = performance.now();
     const deltaTime = (currentTime - lastFrameTime) / (1000 / 60);
     lastFrameTime = currentTime;
 
-    const x = mousePos[0] / window.innerWidth * 100;
-    const y = mousePos[1] / window.innerHeight * 100;
+    const x = mousePos[0] / backgroundElement.clientWidth * 100;
+    const y = mousePos[1] / backgroundElement.clientHeight * 100;
     bgGradientPos[0] += (x - bgGradientPos[0]) / 20 * deltaTime;
     bgGradientPos[1] += (y - bgGradientPos[1]) / 20 * deltaTime;
-    bodyElement.style.background = `radial-gradient(circle at ${bgGradientPos[0]}% ${bgGradientPos[1]}%, #003, #001)`;
+    backgroundElement.style.background = `radial-gradient(circle at ${bgGradientPos[0]}% ${bgGradientPos[1]}%, #003, #001)`;
     requestAnimationFrame(backgroundAnimation);
 }
 
